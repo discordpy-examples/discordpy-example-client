54fdc6dc1fda91466b52041db602d20120c7a680
Seiwell <57401916+Seiwell0610@users.noreply.github.com>
2021-03-07 11:16:34 +0900

    お試し

---
+++ bot.py
@@ -0,0 +1,35 @@
+# discord.pyとosというモジュールをimportしている。
+# importについて
+# https://note.nkmk.me/python-import-usage/
+import discord
+import os
+
+# 毎回、discord.Client()を書くのがめんどくさいので、clientの変数に入れている
+# discord.Client()について
+# https://discordpy.readthedocs.io/ja/latest/api.html?highlight=client#discord.Client
+client = discord.Client()
+
+# 必要なやつ
+@client.event
+# Botが起動したときに、実行されるevent
+# ただし、on_readyは何度も呼び出される可能性がある
+async def on_ready():
+    print("起動完了")
+    # インストールされているdiscord.pyのバージョンを表示する
+    # f"{}"(フォーマット済み文字列リテラル)について
+    # https://note.nkmk.me/python-f-strings/
+    print(f"{discord.__version__}")
+    # print("{}".format(discord.__version__))
+
+@client.event
+# discordで何かしらのメッセージが送信されたときに実行されるevent
+# 何かしらのメッセージが送信されたときの情報をmessageという引数(ひきすう)に入れている
+async def on_message(message):
+    # messageの中にあるメッセージの内容が、"det# hello"の時
+    if message.content == "det# hello":
+        # sendについて
+        # https://discordpy.readthedocs.io/ja/latest/api.html?highlight=send#discord.abc.Messageable.send
+        await message.channel.send("Hello World")  # 何らかしらのメッセージが送られたチャンネルに"Hello World"を送信する
+
+# 環境変数からTOKENと一致する名前の項目を読み込んでいる。
+client.run(os.environ["TOKEN"])
